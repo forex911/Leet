a98a975103057a125bb98bc481ffc53117bdc310
F9 <207845669+forex911@users.noreply.github.com>
2026-05-25 11:08:26 +0530

    Create 1086-DivisorGame

---
+++ 1086-DivisorGame/1086-DivisorGame.java
@@ -0,0 +1,11 @@
+// Last updated: 5/25/2026, 11:08:24 AM
+class Solution {
+    public boolean divisorGame(int n) {
+        if(n%2==0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
