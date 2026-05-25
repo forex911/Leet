a030e258c4c200bc88bf93e24139ffc012baed4c
F9 <207845669+forex911@users.noreply.github.com>
2026-05-25 11:09:39 +0530

    Create 518-CoinChangeIi

---
+++ 518-CoinChangeIi/518-CoinChangeIi.java
@@ -0,0 +1,15 @@
+// Last updated: 5/25/2026, 11:09:37 AM
+public class Solution {
+    public int change(int amount, int[] coins) {
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+        
+        for (int coin : coins) {
+            for (int j = coin; j <= amount; j++) {
+                dp[j] += dp[j - coin];
+            }
+        }
+        
+        return dp[amount];
+    }
+}
