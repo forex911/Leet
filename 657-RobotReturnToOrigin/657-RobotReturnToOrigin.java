4922e4248f4d69ab2d8c4caaaad78fe46d6873ea
F9 <207845669+forex911@users.noreply.github.com>
2026-05-25 11:09:22 +0530

    Create 657-RobotReturnToOrigin

---
+++ 657-RobotReturnToOrigin/657-RobotReturnToOrigin.java
@@ -0,0 +1,21 @@
+// Last updated: 5/25/2026, 11:09:20 AM
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int x = 0, y = 0;  
+
+        for (char c : moves.toCharArray()) {
+            switch (c) {
+                case 'U': y += 1; break;
+                case 'D': y -= 1; break;
+                case 'L': x -= 1; break;
+                case 'R': x += 1; break;
+            }
+        }
+
+        if (x == 0 && y == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
