835ed4eed9b49effb81522c23fa3445cd4ade093
F9 <207845669+forex911@users.noreply.github.com>
2026-05-25 11:10:46 +0530

    Create 242-ValidAnagram

---
+++ 242-ValidAnagram/242-ValidAnagram.py
@@ -0,0 +1,10 @@
+# Last updated: 5/25/2026, 11:10:44 AM
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        return sorted(s)==sorted(t)
+        
