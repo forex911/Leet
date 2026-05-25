149453844eab552cd8da77fcd5a51a667ac0c83e
F9 <207845669+forex911@users.noreply.github.com>
2026-05-25 11:10:36 +0530

    Create 258-AddDigits

---
+++ 258-AddDigits/258-AddDigits.java
@@ -0,0 +1,14 @@
+// Last updated: 5/25/2026, 11:10:35 AM
+class Solution {
+    public int addDigits(int num) {
+        if(num==0){
+            return 0;
+        }
+        else if(num%9==0){
+            return 9;
+        }
+        else{
+            return num%9;
+        }
+    }
+}
