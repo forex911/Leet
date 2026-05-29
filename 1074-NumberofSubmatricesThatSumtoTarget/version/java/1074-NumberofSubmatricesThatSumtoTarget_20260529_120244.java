4c043fc47908fb488e93549664cd90169db55110
F9 <207845669+forex911@users.noreply.github.com>
2026-05-29 12:02:46 +0530

    Create 1074-NumberofSubmatricesThatSumtoTarget

---
+++ 1074-NumberofSubmatricesThatSumtoTarget/version/java/1074-NumberofSubmatricesThatSumtoTarget_20260529_120244.java
@@ -0,0 +1,11 @@
+// Last updated: 5/29/2026, 12:02:44 PM
+1class Solution {
+2    public boolean divisorGame(int n) {
+3        if(n%2==0){
+4            return true;
+5        }
+6        else{
+7            return false;
+8        }
+9    }
+10}
