748c2ec6cdf5b3074e79a79e414bb3bf4a4838d1
F9 <207845669+forex911@users.noreply.github.com>
2026-07-15 10:29:02 +0530

    Create 3658-GCDofOddandEvenSums

---
+++ 3658-GCDofOddandEvenSums/version/java/3658-GCDofOddandEvenSums_20260715_102901.java
@@ -0,0 +1,14 @@
+// Last updated: 7/15/2026, 10:29:01 AM
+1class Solution {
+2    public int gcdOfOddEvenSums(int n) {
+3        // int[] odds=new int[n];
+4        // int[] even=new int[n];
+5        // odds[0]=1;
+6        // even[0]=2;
+7        // for(int i=1;i<n;i++){
+8        //     odds[i]+=odds[i-1]+2;
+9        //     even[i]+=even[i-1]+2;
+10        // }
+11        return n;
+12    }
+13}
