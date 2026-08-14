5192fc027ef2915f038feb107284fdf460391fba
F9 <207845669+forex911@users.noreply.github.com>
2026-08-14 10:44:47 +0530

    Create 1428-JumpGameIii

---
+++ 1428-JumpGameIii/1428-JumpGameIii.java
@@ -0,0 +1,14 @@
+// Last updated: 8/14/2026, 10:44:45 AM
+class Solution {
+    public boolean canReach(int[] arr, int start) {
+        if (start < 0 || start >= arr.length || arr[start] < 0) {
+            return false;
+        }
+        if (arr[start] == 0) {
+            return true;
+        }
+        int jumpDistance = arr[start];
+        arr[start] = -1; 
+        return canReach(arr, start + jumpDistance) || canReach(arr, start - jumpDistance);
+    }
+}
