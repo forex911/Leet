cd448494074f4c40433abaf4dc19584885371ad6
F9 <207845669+forex911@users.noreply.github.com>
2026-08-14 10:39:53 +0530

    File updated at 8/14/2026, 10:39:52 AM

--- 2-AddTwoNumbers/2-AddTwoNumbers.java
+++ 2-AddTwoNumbers/2-AddTwoNumbers.java
@@ -1,28 +1,38 @@
-// Last updated: 5/25/2026, 11:13:45 AM
-class Solution {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode dummyHead = new ListNode(0);
-        ListNode tail = dummyHead;
-        int carry = 0;
-
-        while (l1 != null || l2 != null || carry != 0) {
-            int digit1 = (l1 != null) ? l1.val : 0;
-            int digit2 = (l2 != null) ? l2.val : 0;
-
-            int sum = digit1 + digit2 + carry;
-            int digit = sum % 10;
-            carry = sum / 10;
-
-            ListNode newNode = new ListNode(digit);
-            tail.next = newNode;
-            tail = tail.next;
-
-            l1 = (l1 != null) ? l1.next : null;
-            l2 = (l2 != null) ? l2.next : null;
-        }
-
-        ListNode result = dummyHead.next;
-        dummyHead.next = null;
-        return result;
-    }
-}
+// Last updated: 8/14/2026, 10:39:52 AM
+1class Solution {
+2
+3    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+4
+5        ListNode result = new ListNode(0);
+6
+7        ListNode dummy = result;
+8
+9        int carry = 0;
+10
+11        while (l1 != null || l2 != null || carry != 0) {
+12
+13            int n = (l1 != null) ? l1.val : 0;
+14            int m = (l2 != null) ? l2.val : 0;
+15
+16            int sum = n + m + carry;
+17
+18            carry = sum / 10;
+19
+20            int digit = sum % 10;
+21
+22            dummy.next = new ListNode(digit);
+23
+24            dummy = dummy.next;
+25
+26            if (l1 != null) {
+27                l1 = l1.next;
+28            }
+29
+30            if (l2 != null) {
+31                l2 = l2.next;
+32            }
+33        }
+34
+35        return result.next;
+36    }
+37}
