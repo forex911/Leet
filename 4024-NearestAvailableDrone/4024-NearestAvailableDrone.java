ae3821d823c0d985402675efcccdd06714673359
F9 <207845669+forex911@users.noreply.github.com>
2026-08-22 12:25:39 +0530

    File updated at 8/22/2026, 12:25:33 PM

--- 4024-NearestAvailableDrone/4024-NearestAvailableDrone.java
+++ 4024-NearestAvailableDrone/4024-NearestAvailableDrone.java
@@ -1,17 +1,21 @@
-// Last updated: 8/22/2026, 10:07:28 AM
+// Last updated: 8/22/2026, 12:25:33 PM
 1class Solution {
-2    public int minPenalty(int period, int[] lights, int[] arrivalTime) {
-3        int ans=0;
-4        int maxi=Integer.MIN_VALUE;
-5        for(int j:lights){
-6            maxi=Math.max(maxi,j);
-7        }
-8        for(int i=0;i<arrivalTime.length;i++){
-9            int x=arrivalTime[i]%period;
-10            if(x<maxi) continue;
-11            ans=Math.max(ans,period-x);
-12        }
-13        System.out.println(maxi);
-14        return ans;
-15    }
-16}
+2    public int maximumGap(String s, String st) {
+3        int n=s.length(),m=st.length();
+4        int[] l=new int[n];
+5        int[] r=new int[n];
+6        int j=0;
+7        for(int i=0;i<n;i++){
+8            while(st.charAt(j)!=s.charAt(i)) j++;
+9            l[i]=j++;
+10        }
+11        j=m-1;
+12        for(int i=n-1;i>=0;i--){
+13            while(st.charAt(j)!=s.charAt(i))j--;
+14            r[i]=j--;
+15        }
+16        int ans=0;
+17        for(int i=1;i<n;i++)ans=Math.max(ans,r[i]-l[i-1]);  
+18        return ans;
+19    }
+20}
