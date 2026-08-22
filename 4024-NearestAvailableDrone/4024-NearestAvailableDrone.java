936a4fbaab9fd62a84d0648e942cf33679e19339
F9 <207845669+forex911@users.noreply.github.com>
2026-08-22 09:40:26 +0530

    File updated at 8/22/2026, 9:40:21 AM

--- 4024-NearestAvailableDrone/4024-NearestAvailableDrone.java
+++ 4024-NearestAvailableDrone/4024-NearestAvailableDrone.java
@@ -1,18 +1,20 @@
-// Last updated: 8/22/2026, 9:39:39 AM
-1class Solution {
-2    public int nearestDrone(int[][] drones, int[] target) {
-3        int ans=-1;
-4        int mini=Integer.MAX_VALUE;
-5        for(int i=0;i<drones.length;i++){
-6            int x=Math.abs(drones[i][0]-target[0])+Math.abs(drones[i][1]-target[1]);
-7            System.out.print(x);
-8            if(x<=drones[i][2]){
-9                if(x<mini){
-10                    ans=i;
-11                    mini=Math.min(mini,x);
-12                }
-13            }
-14        }
-15        return ans;
-16    }
-17}
+// Last updated: 8/22/2026, 9:40:21 AM
+class Solution {
+    public int nearestDrone(int[][] drones, int[] target) {
+        int n=drones.length;
+        int index=1000;
+        int min=1000;
+        for(int i=0; i<n; i++){
+            int len=Math.abs(drones[i][0]-target[0])+Math.abs(drones[i][1]-target[1]);
+            if(len<=drones[i][2]){
+                if(len<min){
+                min=len;
+                index=i;
+            }
+            }
+           
+        }
+        return (min==1000)?-1:index;
+        
+    }
+}
