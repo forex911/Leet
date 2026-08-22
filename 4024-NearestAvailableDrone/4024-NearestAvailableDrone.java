55b1f71a4b257a23855cee748ede7061a78994c8
F9 <207845669+forex911@users.noreply.github.com>
2026-08-22 10:07:34 +0530

    File updated at 8/22/2026, 10:07:28 AM

--- 4024-NearestAvailableDrone/4024-NearestAvailableDrone.java
+++ 4024-NearestAvailableDrone/4024-NearestAvailableDrone.java
@@ -1,20 +1,17 @@
-// Last updated: 8/22/2026, 9:40:21 AM
-class Solution {
-    public int nearestDrone(int[][] drones, int[] target) {
-        int n=drones.length;
-        int index=1000;
-        int min=1000;
-        for(int i=0; i<n; i++){
-            int len=Math.abs(drones[i][0]-target[0])+Math.abs(drones[i][1]-target[1]);
-            if(len<=drones[i][2]){
-                if(len<min){
-                min=len;
-                index=i;
-            }
-            }
-           
-        }
-        return (min==1000)?-1:index;
-        
-    }
-}
+// Last updated: 8/22/2026, 10:07:28 AM
+1class Solution {
+2    public int minPenalty(int period, int[] lights, int[] arrivalTime) {
+3        int ans=0;
+4        int maxi=Integer.MIN_VALUE;
+5        for(int j:lights){
+6            maxi=Math.max(maxi,j);
+7        }
+8        for(int i=0;i<arrivalTime.length;i++){
+9            int x=arrivalTime[i]%period;
+10            if(x<maxi) continue;
+11            ans=Math.max(ans,period-x);
+12        }
+13        System.out.println(maxi);
+14        return ans;
+15    }
+16}
