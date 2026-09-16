3a863b8d6bdd1a5baa6d4f49f5fd4dfc42be0a79
F9 <207845669+forex911@users.noreply.github.com>
2026-09-16 14:04:58 +0530

    Create 1025-DivisorGame

---
+++ 1025-DivisorGame/1025-DivisorGame.java
@@ -0,0 +1,11 @@
+// Last updated: 9/16/2026, 2:04:56 PM
+class Solution {
+    public boolean divisorGame(int n) {
+        if(n%2==0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
