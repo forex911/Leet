77b8d6b42d42ded2cfb64d4ec0d46b7aabcf3848
F9 <207845669+forex911@users.noreply.github.com>
2026-09-16 14:00:46 +0530

    Create 3014-MinimumNumberOfPushesToTypeWordI

---
+++ 3014-MinimumNumberOfPushesToTypeWordI/3014-MinimumNumberOfPushesToTypeWordI.java
@@ -0,0 +1,9 @@
+// Last updated: 9/16/2026, 2:00:44 PM
+class Solution {
+    public int minimumPushes(String word) {
+        int n = word.length();
+        int blocks = n / 8;
+        return (blocks * (blocks + 1) * 4)
+                + (n % 8) * (blocks + 1);
+    }
+}
