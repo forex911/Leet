0e35beba28d1c9e7e8aae3a88ddae1e47ab32367
F9 <207845669+forex911@users.noreply.github.com>
2026-09-16 14:04:25 +0530

    Create 1189-MaximumNumberOfBalloons

---
+++ 1189-MaximumNumberOfBalloons/1189-MaximumNumberOfBalloons.java
@@ -0,0 +1,20 @@
+// Last updated: 9/16/2026, 2:04:24 PM
+class Solution {
+    public int maxNumberOfBalloons(String text) {
+        int b=0;
+        int a=0;
+        int l=0;
+        int o=0;
+        int n=0;
+        for (char c : text.toCharArray()) {
+            switch (c) {
+                case 'b': b++; break;
+                case 'a': a++; break;
+                case 'l': l++; break;
+                case 'o': o++; break;
+                case 'n': n++; break;
+            }
+        }
+        return Math.min(b,Math.min(a,Math.min(n,Math.min(l/2,o/2))));
+    }
+}
