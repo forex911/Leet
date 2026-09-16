1d61377bb89576205b00a08370fa6934b2752a4b
F9 <207845669+forex911@users.noreply.github.com>
2026-09-16 14:02:44 +0530

    Create 1784-CheckIfBinaryStringHasAtMostOneSegmentOfOnes

---
+++ 1784-CheckIfBinaryStringHasAtMostOneSegmentOfOnes/1784-CheckIfBinaryStringHasAtMostOneSegmentOfOnes.java
@@ -0,0 +1,19 @@
+// Last updated: 9/16/2026, 2:02:42 PM
+class Solution {
+    public boolean checkOnesSegment(String s) {
+        
+        int idx=0;
+
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i)=='1'){                
+                if(i-idx>1){
+                    return false;
+                }
+                idx=i;
+            }
+        }
+
+        return true;
+
+    }
+}
