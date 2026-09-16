cebf19fd78e3de124f1574fc1e23fdb7f77b1ddf
F9 <207845669+forex911@users.noreply.github.com>
2026-09-16 14:04:11 +0530

    Create 1343-NumberOfSubArraysOfSizeKAndAverageGreaterThanOrEqualToThreshold

---
+++ 1343-NumberOfSubArraysOfSizeKAndAverageGreaterThanOrEqualToThreshold/1343-NumberOfSubArraysOfSizeKAndAverageGreaterThanOrEqualToThreshold.java
@@ -0,0 +1,20 @@
+// Last updated: 9/16/2026, 2:04:09 PM
+class Solution {
+    public int numOfSubarrays(int[] arr, int k, int threshold) {
+        int count=0;
+        int sum=0;
+        for(int i=0;i<k;i++){
+            sum+=arr[i];
+        }
+        if(sum/k>=threshold){
+            count++;
+        }
+        for(int i=k;i<arr.length;i++){
+            sum+=arr[i]-arr[i-k];
+            if(sum/k>=threshold){
+            count++;
+        }
+        }
+        return count;
+    }
+}
