997c631491040a01f54746d684939172ff2840d0
F9 <207845669+forex911@users.noreply.github.com>
2026-09-16 14:04:46 +0530

    Create 1109-CorporateFlightBookings

---
+++ 1109-CorporateFlightBookings/1109-CorporateFlightBookings.java
@@ -0,0 +1,22 @@
+// Last updated: 9/16/2026, 2:04:44 PM
+
+class Solution {
+    public int[] corpFlightBookings(int[][] bookings, int n) {
+        int[] diff = new int[n + 1];
+        for (int i = 0; i < bookings.length; i++) {
+            int start = bookings[i][0];
+            int end = bookings[i][1];
+            int val = bookings[i][2];
+            diff[start] += val;
+            if (end + 1 <= n) {
+                diff[end + 1] -= val;
+            }
+        }
+        int[] res = new int[n];
+        res[0] = diff[1];
+        for (int i = 1; i < n; i++) {
+            res[i] = res[i - 1] + diff[i + 1];
+        }
+        return res;
+    }
+}
