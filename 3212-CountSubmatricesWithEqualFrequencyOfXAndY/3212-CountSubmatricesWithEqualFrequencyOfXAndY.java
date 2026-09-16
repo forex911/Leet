6d270af7b747da606f6efb472e368f42a4282cd2
F9 <207845669+forex911@users.noreply.github.com>
2026-09-16 14:00:39 +0530

    Create 3212-CountSubmatricesWithEqualFrequencyOfXAndY

---
+++ 3212-CountSubmatricesWithEqualFrequencyOfXAndY/3212-CountSubmatricesWithEqualFrequencyOfXAndY.java
@@ -0,0 +1,26 @@
+// Last updated: 9/16/2026, 2:00:37 PM
+class Solution {
+    public int numberOfSubmatrices(char[][] grid) {
+        int clen = grid[0].length;
+        int[] psum = new int[clen];
+        int leftmostXCol = clen;
+        int res = 0;
+
+        for (char[] row : grid) {
+            int csum = 0;
+            for (int c = 0; c < clen; ++c) {
+                char ch = row[c];
+                if (ch == 'X') {
+                    csum++;
+                    if (c < leftmostXCol) leftmostXCol  = c; 
+                } else if (ch == 'Y') {
+                    csum--;
+                }
+
+                if ((psum[c] += csum) == 0 && c >= leftmostXCol) ++res;
+            }
+        }
+
+        return res;
+    }
+}
