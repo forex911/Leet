8a8d1f860e1f308d916bfe26a94532f4f75c16da
F9 <207845669+forex911@users.noreply.github.com>
2026-09-16 14:01:47 +0530

    Create 2302-CountSubarraysWithScoreLessThanK

---
+++ 2302-CountSubarraysWithScoreLessThanK/2302-CountSubarraysWithScoreLessThanK.java
@@ -0,0 +1,23 @@
+// Last updated: 9/16/2026, 2:01:45 PM
+class Solution {
+    public long countSubarrays(int[] nums, long k) {
+        int start = 0;
+        long sum = 0;
+        long count = 0;
+        
+        for (int end = 0; end < nums.length; end++) {
+            sum += nums[end];
+            
+           
+            while (sum * (end - start + 1) >= k) {
+                sum -= nums[start];
+                start++;
+            }
+            
+          
+            count += (end - start + 1);
+        }
+        
+        return count;
+    }
+}
