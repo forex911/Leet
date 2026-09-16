d4dd4543b590ffd0b6cccf540b2e66f4be5d38ea
F9 <207845669+forex911@users.noreply.github.com>
2026-09-16 14:03:33 +0530

    Create 1502-CanMakeArithmeticProgressionFromSequence

---
+++ 1502-CanMakeArithmeticProgressionFromSequence/1502-CanMakeArithmeticProgressionFromSequence.java
@@ -0,0 +1,11 @@
+// Last updated: 9/16/2026, 2:03:31 PM
+class Solution {
+    public boolean canMakeArithmeticProgression(int[] arr) {
+        Arrays.sort(arr);
+        int diff = arr[1] - arr[0];
+        for (int i = 2; i < arr.length; i++) {
+            if (arr[i] - arr[i - 1] != diff) return false;
+        }
+        return true;
+    }
+}
