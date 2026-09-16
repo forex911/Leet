e82e0ca78f599f928e5fa98194872c59c61a9f78
F9 <207845669+forex911@users.noreply.github.com>
2026-09-16 14:02:57 +0530

    Create 1727-LargestSubmatrixWithRearrangements

---
+++ 1727-LargestSubmatrixWithRearrangements/1727-LargestSubmatrixWithRearrangements.java
@@ -0,0 +1,22 @@
+// Last updated: 9/16/2026, 2:02:55 PM
+class Solution {
+    public int largestSubmatrix(int[][] matrix) {
+        int m=matrix.length;
+        int n=matrix[0].length;
+        for(int i=1;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j]==0) continue;
+                matrix[i][j]+=matrix[i-1][j];
+            }
+        }
+        int area=0;
+        for(int i=0;i<m;i++){
+            Arrays.sort(matrix[i]);
+            for(int j=0;j<n;j++){
+                int width=n-j;
+                area=Math.max(area,width*matrix[i][j]);
+            }
+        }
+        return area;
+    }
+}
