40df556de1f574bee6bdfe22b0a3317bd7642689
F9 <207845669+forex911@users.noreply.github.com>
2026-09-16 14:02:27 +0530

    Create 1871-JumpGameVii

---
+++ 1871-JumpGameVii/1871-JumpGameVii.java
@@ -0,0 +1,21 @@
+// Last updated: 9/16/2026, 2:02:25 PM
+class Solution {
+    public boolean canReach(String s, int minJump, int maxJump) {
+        
+        boolean[] dp=new boolean[s.length()];
+        int reached=0;
+        dp[0]=true;
+        for(int i=1;i<s.length();i++){
+            if(i-minJump>=0 && dp[i-minJump]){
+                reached++;
+            }
+            if(i-maxJump-1>=0 && dp[i-maxJump-1]){
+                reached--;
+            }
+            if(reached>0 && s.charAt(i)=='0'){
+                dp[i]=true;
+            }
+        }
+        return dp[s.length()-1];
+    }
+}
