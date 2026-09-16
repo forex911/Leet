0b52c1954ffbde4d10ea012e1cdd7f44e6258e40
F9 <207845669+forex911@users.noreply.github.com>
2026-09-16 13:59:22 +0530

    Create 3754-ConcatenateNonZeroDigitsAndMultiplyBySumI

---
+++ 3754-ConcatenateNonZeroDigitsAndMultiplyBySumI/3754-ConcatenateNonZeroDigitsAndMultiplyBySumI.java
@@ -0,0 +1,25 @@
+// Last updated: 9/16/2026, 1:59:20 PM
+class Solution {
+    public long sumAndMultiply(int n) {
+        String tem = Integer.toString(n);
+        StringBuilder adder = new StringBuilder();
+        long sum = 0;
+
+        for (char x : tem.toCharArray()) {
+            int digit = x - '0';
+
+            if (digit != 0) {
+                adder.append(x);
+                sum += digit;
+            }
+        }
+
+        if (adder.length() == 0) {
+            return 0;
+        }
+
+        long concatenatedValue = Long.parseLong(adder.toString());
+
+        return concatenatedValue * sum;
+    }
+}
