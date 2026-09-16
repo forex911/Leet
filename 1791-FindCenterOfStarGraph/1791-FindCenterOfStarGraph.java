3f6ca1190f93d8a3d58162ab81241065cd67a696
F9 <207845669+forex911@users.noreply.github.com>
2026-09-16 14:02:43 +0530

    Create 1791-FindCenterOfStarGraph

---
+++ 1791-FindCenterOfStarGraph/1791-FindCenterOfStarGraph.java
@@ -0,0 +1,14 @@
+// Last updated: 9/16/2026, 2:02:41 PM
+class Solution {
+    public int findCenter(int[][] edges) {
+        int a = edges[0][0];
+        int b = edges[0][1];
+        int c = edges[1][0];
+        int d = edges[1][1];
+
+        if (a == c || a == d) {
+            return a;
+        }
+        return b;
+    }
+}
