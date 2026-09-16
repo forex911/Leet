8785c4c26ef989de54745b1e1568ebe9c5cdceff
F9 <207845669+forex911@users.noreply.github.com>
2026-09-16 14:06:23 +0530

    Create 826-MostProfitAssigningWork

---
+++ 826-MostProfitAssigningWork/826-MostProfitAssigningWork.java
@@ -0,0 +1,25 @@
+// Last updated: 9/16/2026, 2:06:21 PM
+class Solution {
+    public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
+        int n=difficulty.length;
+        int [][] jobs=new int[n][2];
+        for(int i=0;i<n;i++){
+            jobs[i][0]=difficulty[i];
+            jobs[i][1]=profit[i];
+        }
+        Arrays.sort(jobs, (a, b) -> a[0] - b[0]);
+        Arrays.sort(worker);
+        int max=0;
+        int ind=0;
+        int res=0;
+        for(int i=0;i<worker.length;i++){
+            
+            while (ind < n && jobs[ind][0] <= worker[i]) {
+                max = Math.max(max, jobs[ind][1]);
+                ind++;
+            }
+            res+=max;
+        }
+        return res;
+    }
+}
