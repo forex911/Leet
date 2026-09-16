3c3ce6d4c32a63149c49217f0fb06096bf7df608
F9 <207845669+forex911@users.noreply.github.com>
2026-09-16 14:04:36 +0530

    Create 1299-ReplaceElementsWithGreatestElementOnRightSide

---
+++ 1299-ReplaceElementsWithGreatestElementOnRightSide/1299-ReplaceElementsWithGreatestElementOnRightSide.java
@@ -0,0 +1,17 @@
+// Last updated: 9/16/2026, 2:04:35 PM
+class Solution {
+    public int[] replaceElements(int[] arr) {
+        int max=-1;
+        for(int i=arr.length-1;i>=0;i--){
+            if(arr[i]>max){
+                int temp=arr[i];
+                arr[i]=max;
+                max=temp;
+            }
+            else{
+                arr[i]=max;
+            }
+                }        
+        return arr;
+    }
+}
