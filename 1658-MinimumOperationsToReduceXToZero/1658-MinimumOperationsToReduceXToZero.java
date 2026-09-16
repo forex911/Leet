544d32a99258a6ca61de9f7b22b07fa7923d0f0d
F9 <207845669+forex911@users.noreply.github.com>
2026-09-16 14:03:11 +0530

    Create 1658-MinimumOperationsToReduceXToZero

---
+++ 1658-MinimumOperationsToReduceXToZero/1658-MinimumOperationsToReduceXToZero.java
@@ -0,0 +1,24 @@
+// Last updated: 9/16/2026, 2:03:09 PM
+class Solution {
+    public int minOperations(int[] nums, int x) {
+        int total = 0;
+        for(int num : nums) total += num;
+        int target = total - x;
+        if(target < 0) return -1;
+        if(target == 0) return nums.length;
+        int left = 0;
+        int sum = 0;
+        int maxLen = -1;
+        for(int right = 0; right < nums.length; right++){
+            sum += nums[right];
+            while(sum > target){
+                sum -= nums[left];
+                left++;
+            }
+            if(sum == target){
+                maxLen = Math.max(maxLen, right - left + 1);
+            }
+        }
+        return maxLen == -1 ? -1 : nums.length - maxLen;
+    }
+}
