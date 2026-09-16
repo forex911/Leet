ffc49de539b5f1834894affb810c5136ea836ff7
F9 <207845669+forex911@users.noreply.github.com>
2026-09-16 14:06:11 +0530

    Create 852-PeakIndexInAMountainArray

---
+++ 852-PeakIndexInAMountainArray/852-PeakIndexInAMountainArray.java
@@ -0,0 +1,17 @@
+// Last updated: 9/16/2026, 2:06:09 PM
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+      int st=0;
+      int end=arr.length-1;
+      while(st<end){
+        int mid=(st+end)/2;
+        if(arr[mid]>arr[mid+1]){
+            end=mid;
+        }
+        else{
+            st=mid+1;
+        }
+      }
+      return end;
+}
+}
