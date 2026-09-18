ffb54ebed29af81e5434f590a0600f99eb767251
F9 <207845669+forex911@users.noreply.github.com>
2026-09-18 09:32:57 +0530

    File updated at 9/18/2026, 9:32:56 AM

--- 42-TrappingRainWater/42-TrappingRainWater.java
+++ 42-TrappingRainWater/42-TrappingRainWater.java
@@ -1,27 +1,23 @@
-// Last updated: 5/25/2026, 11:13:00 AM
-class Solution {
-    public int trap(int[] height) {
-        int n = height.length;
-        int left = 0, right = n-1;
-        int leftMax = 0, rightMax = 0;
-        int res = 0;
-        while (left <= right) {
-            if (height[left] <= height[right]) {
-                if (height[left] >= leftMax) {
-                    leftMax = height[left];
-                } else {
-                    res += leftMax - height[left];
-                }
-                left++;
-            } else {
-                if (height[right] >= rightMax) {
-                    rightMax = height[right];
-                } else {
-                    res += rightMax - height[right];
-                }
-                right--;
-            }
-        }
-        return res;
-    }
-}
+// Last updated: 9/18/2026, 9:32:56 AM
+1class Solution {
+2    public int trap(int[] height) {
+3        int n=height.length;
+4        int[] left=new int[n];
+5        int[] right=new int[n];
+6        int currmax=Integer.MIN_VALUE;
+7        for(int i=0;i<n;i++){
+8            currmax=Math.max(currmax,height[i]);
+9            left[i]=currmax;
+10        }
+11        currmax=Integer.MIN_VALUE;
+12        for(int i=n-1;i>=0;i--){
+13            currmax=Math.max(currmax,height[i]);
+14            right[i]=currmax;
+15        }
+16        int ans=0;
+17        for(int i=0;i<n;i++){
+18            ans+=Math.min(right[i],left[i])-height[i];
+19        }
+20        return ans;
+21    }
+22}
